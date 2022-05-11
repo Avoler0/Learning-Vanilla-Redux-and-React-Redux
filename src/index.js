89a83d0ad7a5f6602a88da0243cc8f74f34dd27d
Avoler0 <jys1391@gmail.com>
2022-05-11 18:43:40 +0900

    기초 끝

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { createStore } from "redux";
+
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+
+const countModifier = (count = 0 , action) => {
+  switch(action.type){
+    case "ADD": return count+1;
+    case "MINUS": return count-1;
+    default: return count;
+  }
+}
+const countStore = createStore(countModifier);
+
+
+
+
+add.addEventListener("click" , () => countStore.dispatch({type:"ADD"}))
+minus.addEventListener("click" , () => countStore.dispatch({type:"MINUS"}))
+const onChange = () => {
+  number.innerText = countStore.getState()
+}
+countStore.subscribe(onChange)
